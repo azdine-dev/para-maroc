35b75fec3820577fbc18ac168ecad931672b58e4
azdine-dev <azeddinebenhalima@gmail.com>
2022-11-05 16:08:40 +0100

    change env url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   demo: 'difussion',
   phone: 212623281684,
-  SERVER_URL: 'http://localhost:1337',
+  SERVER_URL: 'https://difussion-api.herokuapp.com',
 };
 
 /*
